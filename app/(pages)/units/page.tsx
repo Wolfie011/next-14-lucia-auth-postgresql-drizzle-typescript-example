e3da3d0e67a133bf0ac2383f710de407b17150b5
Wolfie011 <krafcik30@gmail.com>
2024-10-31 16:54:12 +0100

    xd

---
+++ app/(pages)/units/page.tsx
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { validateRequest } from "@/lib/lucia";
+import { redirect } from "next/navigation";
+
+import { columns } from "./columns";
+import { userSchemaAdditional } from "@/types/index";
+import { DataTable } from "@/components/table/data-table";
+import { getUsers } from "@/actions/user.actions";
+
+// async function getUsers(page: number, perPage: number) {
+//   const res = await fetch(`https://jsonplaceholder.org/users`)
+//   const apiUsers = await res.json()
+
+//   // Transforming API response to match User type
+//   const users = apiUsers.map((apiUser: any) => ({
+//     id: apiUser.id.toString(),
+//     username: apiUser.login.username,
+//     firstname: apiUser.firstname,
+//     lastname: apiUser.lastname,
+//     email: apiUser.email,
+//     phone: apiUser.phone,
+//     role: "user", // You might want to adjust this based on actual data
+//     status: "active", // You might want to adjust this based on actual data
+//   }));
+
+//   return z.array(userSchemaAdditional).parse(users)
+// }
+
+export default async function UnitPage() {
+  // const data = await getData()
+  const data = await getUsers();
+  const { user } = await validateRequest();
+
+  if (!user) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="p-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
